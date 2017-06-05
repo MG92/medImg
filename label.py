ca9e0807a14bf6bf38efd0be2eee5a70ce3a4deb
Melissa Gourlay <melissa.gourlay@hotmail.co.uk>
2017-06-05 20:48:50 +0100

    Prevent writing over old data

--- label.py
+++ label.py
@@ -10,7 +10,6 @@ from PIL import Image
 
 data_dir = './brain/t1_patches'
 sample_dir = './brain/sample_patches'
-class_list =  open("sample_class.txt",'r+')
 
 def scale_img(slic):
     max_val=0
@@ -46,32 +45,49 @@ def sample_patches():
             f.close()
     return 0;
 
-def create_label():
+def create_label(existing_list):
     sample_patches = os.listdir(sample_dir)
-    sample_list = {}
-    print("Classify each image as: \n 0 = NO anomaly \n 1 = anomaly present")
+    '''if len(existing_list)==0:
+        sample_list={}
+    else:
+        sample_list = existing_list #avoid classifing items already in list'''
     i=0
     for patch in sample_patches:
         if (patch.endswith('.png')):
-            i+=1
-            full_name = os.path.join(data_dir,patch)
-            img = np.array(Image.open(full_name))
-            print ("Showing image {}".format(i))
-            plt.imshow(img,cmap='gray')
-            plt.show()
-            val = raw_input("Select either 0= NO anomaly or 1=anomaly present: \n")
-            if (int(val)==0 or int(val)==1):
-                sample_list[patch] = val
-            if(i%10==0):#every time 10 more classifications made, save dictionary
-                json.dump(sample_list, class_list)
+            print patch
+            if (patch in existing_list):
+                print('already exists')
+                continue
+            elif(patch not in existing_list):
+                #print("Classify each image as: \n 0 = NO anomaly \n 1 = anomaly present")
+                i+=1
+                full_name = os.path.join(data_dir,patch)
+                img = np.array(Image.open(full_name))
+                print ("Showing image {}".format(i))
+                plt.title(patch)
+                plt.imshow(img,cmap='gray')
+                plt.show()
+                val = raw_input("Select either 0=NO anomaly; 1=potential anomaly; or\
+                 2=anomaly present: \n")
+                if (int(val)==0 or int(val)==1 or int(val)==2):
+                    existing_list[patch] = val
+            if(i%10==0):  #every time 10 more classifications made, save dictionary
+                with open("sample_class.txt",'w') as class_list:
+                    json.dump(existing_list, class_list)
     return 0;
 
 def query_label():
     sample_patches = os.listdir(sample_dir)
-    classes = json.load(class_list)
-    for patch in sample_patches:
-        label = int(classes[patch])
-        print label
+    try:
+        with open("sample_class.txt",'r') as class_list:
+            classes = json.load(class_list)
+    except:
+        return {}
+    '''for i in range(len(classes)):
+        label = classes[sample_patches[i]]
+        #print label'''
+    print('len classes: ',len(classes))
+    return classes
 
-#create_label()
-#query_label()
+labels = query_label() #query label first to retrieve answers already submitted
+create_label(labels)
